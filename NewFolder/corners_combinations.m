f1ce6112d11b6d3802047417c8e694204574646d
Mjoym <joannamoladf@gmail.com>
2024-07-24 13:40:51 +0300

    The other file inside the folder commit

---
+++ NewFolder/corners_combinations.m
@@ -0,0 +1,27 @@
+function [corners, comb] = corners_combinations(stim_ind)
+% Create all possible combinations for collinearity check (between corners):
+
+% Find max and min (corners):
+rowMax = find(stim_ind(:,1)==max(stim_ind(:,1)));
+rowMin = find(stim_ind(:,1)==min(stim_ind(:,1)));
+bottomRight = [max(stim_ind(:,1)), max(stim_ind(rowMax,2))];
+bottomLeft = [max(stim_ind(:,1)), min(stim_ind(rowMax,2))];
+topRight = [min(stim_ind(:,1)), max(stim_ind(rowMin,2))];
+topLeft = [min(stim_ind(:,1)), min(stim_ind(rowMin,2))];
+
+if bottomLeft == bottomRight
+    if topLeft == topRight
+    corners = [bottomLeft; topLeft];
+    else
+        corners = [bottomLeft; topLeft; topRight];
+    end
+else
+    if topLeft == topRight
+        corners = [bottomLeft; topLeft; bottomRight];
+    else
+        corners = [bottomLeft; topLeft; topRight; bottomRight];
+    end
+end
+
+comb = nchoosek(1:size(corners,1),2);
+end
